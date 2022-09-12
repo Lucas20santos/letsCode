6aa2490c6f9201ee606c39d1b33a3cf5e2f41c60
lucas de souza santos <lucass.santoss210@gmail.com>
2022-09-12 09:56:12 -0300

    subindo

---
+++ Java/helloworld.java
@@ -0,0 +1,13 @@
+package Java;
+
+/**
+ * helloworld
+ */
+public class helloworld 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ dataScience/python/operadores.py
@@ -0,0 +1,46 @@
+"""
+Opeeradores
+
+Operadores Matemáticos
+Operações           Simbolos
+
+- Soma:                 +
+- Subtração:            -
+- Multiplicação:        *
+- Divisão:              /
+- Divisão Inteira:      //
+- Resto da Divisão:     %
+- Potência:             **
+
+Operadores Boleanos
+Operadores          Simbolos
+igual                   ==
+maior que               >
+maior ou igual que      >=
+menor que               <
+menor ou igual que      <=
+negação                  !
+
+"""
+
+num1 = 3
+num2 = 4
+# Exemplos de Operadores Matemáticos
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+
+# Exemplos de Operadores Lógicos
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 != num2)
+print(num1 == num2)
+
+
