450af0c4bd797b246ede81b01cfe7a831d1978b3
lucas de souza santos <lucass.santoss210@gmail.com>
2022-09-12 23:31:18 -0300

    variable in java

---
+++ Java/variaveis.java
@@ -0,0 +1,48 @@
+package Java;
+
+/**
+ * Variáveis
+*/
+
+public class variaveis 
+{
+    public static void main(String[] args) 
+    {
+        /**
+         * 8 tipos primitivos
+         */
+        /**
+         * Tipo Inteiro
+         * */ 
+        byte num1 = 0;
+        short num2 = 0;
+        int num3 = 0;
+        long num4 = 0;
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        
+        /**
+         * Tipo Booleano
+         * */ 
+        boolean valor = true;
+        System.out.println(valor);
+
+        /**
+         * Tipo character
+         * */ 
+        char letra = 'A';
+        System.out.println(letra);
+
+        /**
+         * Tipos flutuante
+        */
+        float real = 3;
+        double real2 = 4;
+        
+        System.out.println (real);
+        System.out.println (real2);
+    }    
+}
